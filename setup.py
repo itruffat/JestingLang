1a9dc98062a93bd345300a6b2711dc87a68e1523
itruffat <34661660+itruffat@users.noreply.github.com>
2023-05-09 12:44:29 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='JestingLang',
+    version='0.0.0a1',
+    author='itrufat',
+    author_email='',
+    description='A compiler for the minimalist spreadsheet language Jesting',
+    long_description='A compiler + a few navigators (including an interpreter) for a minimalist language intended to copy the most basic functionalities found in spreadsheet-applications called JestingLang. It was created to be used with Jesting, a spreadsheet terminal tool.',
+    long_description_content_type='text/markdown',
+    url='https://github.com/your-username/your-repo',
+    packages=['your_package'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    install_requires=[
+        'dependency-package2',
+    ],
+)
