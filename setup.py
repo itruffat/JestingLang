14bae93811278ec0369e001485e77ea45b4e793b
itruffat <34661660+itruffat@users.noreply.github.com>
2023-06-25 21:51:44 +1000

    Fixed setup

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@ from setuptools import setup
 
 setup(
     name='JestingLang',
-    version='0.0.0a6',
+    version='0.0.0a7',
     author='itrufat',
     description='A compiler for the minimalist spreadsheet language Jesting',
     long_description='A compiler + a few node navigators (including an interpreter) for a minimalist language intended to copy the most basic functionalities found in spreadsheet-applications called JestingLang. It was created to be used with Jesting, a spreadsheet terminal tool.',
     long_description_content_type='text/markdown',
     url='https://github.com/itruffat/JestingLang',
-    packages=['JestingLang', 'JestingLang.JParsing', 'JestingLang.JVisitors', 'JestingLang.Misc',
-              'JestingLang.Misc.JLogic', 'JestingLang.Misc.JDereferencer', 'JestingLang.Misc.JExample'],
+    packages=['JestingLang',
+              'JestingLang.Core',
+                  'JestingLang.Core.JParsing', 'JestingLang.Core.JVisitors', 'JestingLang.Core.JDereferencer',
+              'JestingLang.JestingScript',
+                  'JestingLang.JestingScript.JParsing', 'JestingLang.JestingScript.JVisitors',
+                  'JestingLang.JestingScript.JDereferencer',
+              'JestingLang.Misc',
+                  'JestingLang.Misc.JLogic', 'JestingLang.Misc.JTesting'],
     package_dir={'': 'src'},
     classifiers=[
         'Development Status :: 3 - Alpha',
